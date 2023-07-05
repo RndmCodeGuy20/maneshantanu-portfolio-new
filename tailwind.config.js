c873630192d5bf045194756bc74b77a0df87f8a7
maneshantanu20 <manesg@rknec.edu>
2023-07-05 08:41:42 +0530

    bug(PROJECT): custom web font added to index.css [WIP]

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+/**
+ * @font-face {
+ *  font-family: 'Pilcrow Rounded';
+ *  font-style: normal;
+ *  font-weight: 400;
+ *  src: url('../fonts/PilcrowRounded-Regular.woff2') format('woff2'),
+ */
+
+
 export default {
     content: [
         "./index.html",
